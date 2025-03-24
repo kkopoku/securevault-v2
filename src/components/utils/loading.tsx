a4de9267bde51c76e4d6622bf3b8e5e6ba045d04
kkopoku <kk.opoku@outlook.com>
2025-03-24 13:17:55 +0000

    update: loading spinner

--- src/components/utils/loading.tsx
+++ src/components/utils/loading.tsx
@@ -1,5 +1,5 @@
 export default function Loading(){
     return (
-        <div className="rounded-full justify-center animate-spin h-24 w-24 border-[10px] border-b-purple-600 border-gray-300"></div>
+        <div className="rounded-full justify-center animate-spin h-10 w-10 border-[4px] border-b-purple-700 border-purple-200"></div>
     )
 }
